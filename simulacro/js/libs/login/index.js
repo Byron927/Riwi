0f6083607d9345216a1b6907dc68c196ce80ba3f
Byron927 <byronotalvaro92@gmail.com>
2024-02-11 09:37:18 -0500

    guardian

--- simulacro/js/libs/login/index.js
+++ simulacro/js/libs/login/index.js
@@ -19,5 +19,23 @@ async function logIn() {
   const response = await fetch(`${URL_USERS}?email=${email.value}`);
   const data = await response.json();
 
-  console.log(data);
+  if (!data) {
+    console.error("Email no registrado");
+    return;
+  }
+  //Si yo estoy obteniendo por email, entonces estoy seguro de que solo hay un usuario...
+  //...que tiene ese email, por eso utilizo la posición cero
+  if (data[0].password !== password.value) {
+    console.error("Contraseña inválida");
+    return;
+  }
+
+  //localStorage es una api del navegador que me da persistencia, es decir, si cierro el navegador...
+  //...y lo vuelvo a abrir tendré la info en el localStorage, no se elimina a menos de que el usuario...
+  //...o el código la eliminen y siempre recibe dos valores, 1ro la llave y 2do el valor, OJO,...
+  //...solo guarda strings, entonces como yo quiero guardar un objeto me toca convertirlo a string...
+  //...con la función JSON.stringify y dentro del parentesis le envío lo que quiero convertir:
+  localStorage.setItem("user", JSON.stringify(data[0]));
+  //Despues redireccionamos al usuario hacia la vista administrator
+  window.location.href = "administrator.html";
 }
